1be55f13eed982fdfcbbce84f709476d03840854
YICHAEGYEONG <hhh00613@gmail.com>
2021-05-19 04:20:42 +0900

    git init

---
+++ src/test/java/com/example/designpatternstudy/DesignPatternStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.designpatternstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
